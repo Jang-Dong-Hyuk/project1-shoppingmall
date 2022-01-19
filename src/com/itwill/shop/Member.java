2fde18fcb26c402e0a261430c99af81c3bcab7aa
huk77 <huk77@121.166.245.200>
2022-01-19 13:40:06 +0900

    두번쨰 커밋

--- src/com/itwill/shop/Member.java
+++ src/com/itwill/shop/Member.java
@@ -1,5 +1,17 @@
 package com.itwill.shop;
 
 public class Member {
+	private int no;
 
+	public Member() {
+		
+	}
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+	
 }
