7f148a3305d431fb1ec66817d7ee7f687c7bca2c
huk77 <huk77@121.166.245.200>
2022-01-19 13:41:30 +0900

    세번쨰 커밋

--- src/com/itwill/shop/Product.java
+++ src/com/itwill/shop/Product.java
@@ -1,5 +1,21 @@
 package com.itwill.shop;
 
 public class Product {
+	private int no;
 
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public Product(int no) {
+		super();
+		this.no = no;
+	}
+
+	
+	
 }
